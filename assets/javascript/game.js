8b3d9b46456d8fd92aec33c7f5ee92434f7e5a9f
leighkristine <lkroxmob@gmail.com>
2017-09-02 06:15:33 -0700

    working gameplay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,52 @@ var losses = 0
 var userGuesses = []
 
 
+var reset = function() {
+	userGuesses = [];
+	currentScore = 0;
+	// targetScore();
+	// diamondRandom();
+	// pyramidRandom();
+	// eggRandom();
+	// quartzRandom();
+};
+
+var crystals = function() {
+	targetScore = Math.floor(Math.random() * (120 - 19) + 19);
+					console.log("Target score: " + targetScore);
+					$("#total").html(targetScore);
+	diamondRandom = Math.floor(Math.random() * (12 - 1) + 1);
+					console.log("Diamond: " + diamondRandom);
+	pyramidRandom = Math.floor(Math.random() * (12 - 1) + 1);
+					console.log("Pyramid: " + pyramidRandom);
+	eggRandom = Math.floor(Math.random() * (12 - 1) + 1);
+				console.log("Egg: " + eggRandom);
+	quartzRandom = Math.floor(Math.random() * (12 - 1) + 1);
+				console.log("Quartz: " + quartzRandom);
+};
+
+
 $(document).ready(function(game) {
 
-	var targetScore = Math.floor(Math.random() * (120 - 19) + 19);
-			console.log(targetScore);
-			$("#total").html(targetScore);
+	crystals();
 
-	var diamondRandom = Math.floor(Math.random() * (12 - 1) + 1);
-			console.log(diamondRandom);
-	var pyramidRandom = Math.floor(Math.random() * (12 - 1) + 1);
-			console.log(pyramidRandom);
-	var eggRandom = Math.floor(Math.random() * (12 - 1) + 1);
-			console.log(eggRandom);
-	var quartzRandom = Math.floor(Math.random() * (12 - 1) + 1);
-			console.log(quartzRandom);
+	// var targetScore = Math.floor(Math.random() * (120 - 19) + 19);
+	// 		console.log(targetScore);
+	// 		$("#total").html(targetScore);
+
+	// var diamondRandom = Math.floor(Math.random() * (12 - 1) + 1);
+	// 		console.log(diamondRandom);
+	// var pyramidRandom = Math.floor(Math.random() * (12 - 1) + 1);
+	// 		console.log(pyramidRandom);
+	// var eggRandom = Math.floor(Math.random() * (12 - 1) + 1);
+	// 		console.log(eggRandom);
+	// var quartzRandom = Math.floor(Math.random() * (12 - 1) + 1);
+	// 		console.log(quartzRandom);
+
 
 	$("#diamond, #pyramid, #eggcrystal, #quartz").click(function(clicks) {
+
+
 
 		if (this.id == "diamond") {
 			userGuesses.push(diamondRandom);
@@ -43,18 +73,20 @@ $(document).ready(function(game) {
 			};
 
 			//conditionals
-			var wins = 0;
-			var losses = 0;
 
 			if (currentScore === targetScore) {
 				$("#status").html("Yay, you won!")
-				wins++
-				$("#wins").html(wins)
+				wins++;
+				document.querySelector("#wins").innerHTML = wins;
+				reset();
+				crystals();
 			} else if (currentScore > targetScore) {
 				$("#status").html("Boo, you lost!");
-				losses++
+				losses++;
 				console.log(losses);
-				$("#losses").html(losses);
+				document.querySelector("#losses").innerHTML = losses;
+				reset();
+				crystals();
 			} else {
 				console.log("continue");
 			};
