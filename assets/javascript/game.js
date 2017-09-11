c6ad5168e36971ef0c44d1f7797c6c7756fbfebf
dblanchard13 <davidb.prof@gmail.com>
2017-09-10 22:37:49 -0700

    Homework comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+// You'll want to get in the habit of placing all of your client side JS code within a document ready block,
+// an iife (immediately invoked function expression), or some other functional context
+// The reason for this is to not leak variables onto the global scope.
+// In this case, your game variables and all of your game functions are readily
+// available for a malicious user to tamper withm(ie they could set their wins to be 1000000 😮).
+
 var wins = 0
 var losses = 0
 var userGuesses = []
@@ -14,18 +20,29 @@ var reset = function() {
 };
 
 var crystals = function() {
-	targetScore = Math.floor(Math.random() * (120 - 19) + 19);
-					console.log("Target score: " + targetScore);
-					$("#total").html(targetScore);
-	diamondRandom = Math.floor(Math.random() * (12 - 1) + 1);
-					console.log("Diamond: " + diamondRandom);
-	pyramidRandom = Math.floor(Math.random() * (12 - 1) + 1);
-					console.log("Pyramid: " + pyramidRandom);
-	eggRandom = Math.floor(Math.random() * (12 - 1) + 1);
-				console.log("Egg: " + eggRandom);
-	quartzRandom = Math.floor(Math.random() * (12 - 1) + 1);
-				console.log("Quartz: " + quartzRandom);
+	// Since the string of code for generating random numbers
+	// is rather unwieldy I'd suggest placing it in a utility function
+	targetScore = getRandomNum(120, 19)
+
+	// console.log's are awesome for validating your work when developing a program
+	// but it's generally best practice to remove them from production code
+	// console.log("Target score: " + targetScore);
+	// I know it can seem like a trivial detail, but consistent indentation
+	// really does pay dividends towards code maintainability 🙌
+	$("#total").html(targetScore);
+	diamondRandom = getRandomNum(12, 1)
+	// console.log("Diamond: " + diamondRandom);
+	pyramidRandom = getRandomNum(12, 1)
+	// console.log("Pyramid: " + pyramidRandom);
+	eggRandom = getRandomNum(12, 1)
+	// console.log("Egg: " + eggRandom);
+	quartzRandom = getRandomNum(12, 1)
+	// console.log("Quartz: " + quartzRandom);
 };
+
+function getRandomNum (max, min) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
 
 
 $(document).ready(function(game) {
@@ -52,23 +69,31 @@ $(document).ready(function(game) {
 
 		if (this.id == "diamond") {
 			userGuesses.push(diamondRandom);
-			console.log(userGuesses);
+			// console.log(userGuesses);
 		} else if (this.id == "pyramid") {
 			userGuesses.push(pyramidRandom);
-			console.log(userGuesses);
+			// console.log(userGuesses);
 		} else if (this.id == "eggcrystal") {
 			userGuesses.push(eggRandom);
-			console.log(userGuesses);
+			// console.log(userGuesses);
 		} else if (this.id == "quartz") {
 			userGuesses.push(quartzRandom);
-			console.log(userGuesses);
+			// console.log(userGuesses);
 		} else {
-			console.log(error);
+			// this error variable isn't defined anywhere, so this would actually throw 
+			// an `error is not defined` error
+			// console.log(error);
 		};	
+
+		// instead of recalculating the currentScore based on all the values in the 
+		// userGuesses array, it may be a bit more straightforward to keep track of the 
+		// currentScore as a variable within the scope of the document ready block.
+		// That way you'd only need to add the most recent guess to that variable 
+		// to determine the score instead of looping through the array every time.
 		var currentScore = 0
 		for (var i = 0; i < userGuesses.length; i++) {
 				currentScore += userGuesses[i] << 0;
-				console.log(currentScore);
+				// console.log(currentScore);
 				$("#current").html(currentScore);
 			};
 
@@ -83,12 +108,12 @@ $(document).ready(function(game) {
 			} else if (currentScore > targetScore) {
 				$("#status").html("Boo, you lost!");
 				losses++;
-				console.log(losses);
+				// console.log(losses);
 				document.querySelector("#losses").innerHTML = losses;
 				reset();
 				crystals();
 			} else {
-				console.log("continue");
+				// console.log("continue");
 			};
 
 	});
